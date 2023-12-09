69c2f2ae769413ba10aeb65b52dfcce0ceb9baa5
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2023-12-09 18:47:00 +0900

    クラス名を適切な前に変更。テスト手順の説明を追加

---
+++ app/src/test/java/jp/ac/uryukyu/ie/e235750/bombCountTest.java
@@ -0,0 +1,35 @@
+package jp.ac.uryukyu.ie.e235750;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 設置された爆弾の数が期待する爆弾の数と等しいことを検証する
+ * 検証手順
+ *  1.Cellクラスのオブジェクトを作成し、セルの情報を取得。
+ *  2.ループ処理で全てのセルにアクセス。
+ *  3.各セルでisBombメソッドを実行しtrueの場合int型変数bombCounterをインクリメント。
+ *  4.期待する爆弾の数とbombCounterをassertEqualsメソッドで比較する。
+ */
+public class bombCountTest {
+    private int FIELD_ROW         = 5;
+    private int FIELD_COLUMN      = 5;
+    private int NUM_BOMB          = 10;
+    private int bombCounter       = 0;
+
+    @Test
+    void test(){
+        MinesweeperGame game = new MinesweeperGame();
+        Cell gamePanel = new Cell(game);
+        CustomButton[][] buttons = gamePanel.buttons;
+
+        for (int x = 0; x < FIELD_ROW; x++) {
+            for (int y = 0; y < FIELD_COLUMN; y++) {
+                if(buttons[x][y].isBomb()){
+                    bombCounter ++;
+                }                
+            }
+        }
+        assertEquals((int)NUM_BOMB, (int)bombCounter);
+    }
+}
